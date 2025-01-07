003187825b06d1941441012dc26ae063fc706363
Nikesh Pathak <nikspathak91@gmail.com>
2025-01-07 01:34:25 -0500

    Querying database

--- etl_python_gdp.py
+++ etl_python_gdp.py
@@ -54,4 +54,11 @@ def load_to_db(df, sql_connection, table_name):
     ''' This function saves the final dataframe as a database table
     with the provided name. Function returns nothing.'''
     df.to_sql(table_name, sql_connection, if_exists='replace', index = False)
+    
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
 
