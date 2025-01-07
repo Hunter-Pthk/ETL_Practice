e957181de618b5b8748cc38c7c2a76a6f4e0697d
Nikesh Pathak <nikspathak91@gmail.com>
2025-01-07 00:39:58 -0500

    Importing the required libraries

---
+++ etl_python_gdp.py
@@ -0,0 +1,15 @@
+# Code for ETL operations on Country-GDP data
+# Importing the required libraries
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
